9828fd2563d6ffe3c6528208bd79d62f5739577c
dzaky <dzakyrahmanto@gmail.com>
2018-08-22 18:59:23 +0700

    change button text when finish the exam

--- app/src/main/java/com/rifkiannas/tryoutunmatematika/MainActivity.java
+++ app/src/main/java/com/rifkiannas/tryoutunmatematika/MainActivity.java
@@ -113,6 +113,7 @@ public class MainActivity extends AppCompatActivity implements FragmentSoal.OnCa
                         btnNext.setEnabled(false);
                         btnPrev.setEnabled(false);
                         getResetScore();
+                        btnStart.setText("Test again!");
                     }
                 }.start();
             }
